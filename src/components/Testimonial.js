fe28b4b00746cae501c7964a8d4fd25551c92d1d
Feruz <59234162+Feruzteame@users.noreply.github.com>
2023-06-29 13:56:11 +0200

    small update

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -18,7 +18,7 @@ const Testimonial = () => {
     const handleNext = () => {
       setActiveIndex(activeIndex === Testimonial_data.length - 1 ? 0 : activeIndex + 1);
     };
-
+  // slick setting
   const settings = {
     dots: true,
     infinite: true,
@@ -56,15 +56,15 @@ const Testimonial = () => {
             style={{ transform: `translateX(-${activeIndex * 100}%)` }}
           >
             {Testimonial_data.map((item) => (
-              <div key={item.id} className='hidden lg:flex justify-center items-center h-auto lg:m-20 mt-4'>
-                <div className='relative flex flex-col justify-center items-center w-[300px] h-auto lg:w-[500px] lg:h-[300px]'>
-                  <img src={item.image} alt={'person' + item.id} className='absolute mb-10 top-0 lg:top-[-10%] h-24 w-24 lg:shadow-2xl shadow-black rounded-full border-2 border-solid border-white z-20' ></img>
-                  <div className=" bg-white rounded-lg overflow-hidden lg:shadow-2xl shadow-[gray] pt-10 border-2 border-solid border-white h-auto w-full">
-                    <div className='flex flex-col justify-start items-start p-12 lg:px-5 lg:pt-14'>
+              <div key={item.id} className='hidden lg:flex justify-center items-center h-auto p-20  '>
+                <div className='relative flex flex-col justify-center items-center w-[500px] h-auto p-6'>
+                  <img src={item.image} alt={'person' + item.id} className='absolute mb-10 top-[-10%] h-32 w-32 shadow-2xl shadow-[gray] rounded-full border-2 border-solid border-[gray] z-20' ></img>
+                  <div className=" bg-white rounded-lg overflow-hidden shadow-2xl shadow-[gray] pt-10 border-2 border-solid border-white h-auto w-full">
+                    <div className='flex flex-col justify-start items-start px-5 pt-14'>
                       <img src={quote} alt='quote' className='h-10 pr-[90%]'></img>
                       <p className='text-justify w-full'>{item.description}</p>
                     </div>
-                    <p className='px-10 lg:py-5 text-right font-bold italic text-[#ff583e]'>{item.name}</p>
+                    <p className='px-10 py-5 text-right font-bold italic text-[#ff583e]'>{item.name}</p>
                   </div>
                 </div>
               </div>
@@ -78,6 +78,8 @@ const Testimonial = () => {
           <img  src={rightArrow} alt='rightArrow' className='bg-[#ff583e6f] p-2 rounded-full lg:w-10 lg:h-10'/>
         </button>
       </div>
+
+      {/* Small size screen design */}
       <div className='block lg:hidden'>
       <Slider {...settings}>
         {Testimonial_data.map((item) => (
@@ -86,7 +88,7 @@ const Testimonial = () => {
               <img
                 src={item.image}
                 alt={item.id}
-                className="w-60 h-60 object-cover rounded-full"
+                className="w-40 h-40 object-cover rounded-full"
               />
               <div className=" bg-white rounded-lg overflow-hidden lg:shadow-2xl shadow-[gray] border-2 border-solid border-white h-auto w-full">
                 <div className='flex flex-col justify-start items-start p-5 lg:px-5 lg:pt-14'>
