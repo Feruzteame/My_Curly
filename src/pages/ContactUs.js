efa69674fd19d4fe2342a6e038cf5ff07ba7a858
Feruz <59234162+Feruzteame@users.noreply.github.com>
2023-06-30 15:17:15 +0200

    add sending message form

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm, ValidationError } from '@formspree/react';
 
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
@@ -6,8 +8,20 @@ import contactImage from '../images/contact_us.png';
 import call from '../icons/call.svg';
 import email from '../icons/email.svg';
 import location from '../icons/location.svg'
+import whiteLogo from '../images/whiteLogo.png'
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("mrgwzrgb");
+
+  if (state.succeeded) {
+      return <div className='w-full h-auto lg:h-[500px] flex flex-col justify-center items-center gap-5 text-xl'>
+                <img src={whiteLogo} alt="Logo" className="h-14 w-14" />
+                <p className='text-2xl'>Thanks for Contact Us !</p>
+                <p className='text-lg'>Your email is sent successfully, we will back you soon.</p>
+                <Link to="/" className="hover:underline decoration-[#ff583e] decoration-2 text-lg"> Back to website</Link>
+                <p className='italic text-[#ff583e] text-lg'>Team MyCurly</p>
+            </div>
+  }
 
   return (
     <div className='h-screen'>
@@ -34,8 +48,7 @@ const Contact = () => {
             <img src={contactImage} alt='mobile' className='w-[400] h-[500px]'></img>
           </div>
           <form
-          netlify
-          name='myCurly'
+          onSubmit={handleSubmit}
           className='w-[60%] lg:w-[400px]'>
             <div className="mb-4 w-full">
               <label htmlFor="name" className="block font-medium mb-1">
@@ -71,8 +84,7 @@ const Contact = () => {
                 placeholder='Message'
                 rows="6"
                 className="w-full border-gray-300 border-2 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 p-2"
-              ></textarea>
-              <div data-netlify-recaptcha='true'></div>
+              />
             </div>
             <div className="text-center">
               <button className='bg-[#ff583e] rounded w-full px-10 py-2 border hover:bg-white hover:text-black hover:border-[#ff583e]'> 
